279b817b3891ad5fea91f19bd1872024cfca9512
Sagar Deshmukh <sagar@Mac.lan>
2025-11-22 20:33:45 +0530

    first commit - Lets goo

---
+++ readme.md
@@ -0,0 +1,1 @@
+The purpose of this repository is to explore git commands and learn git
---
+++ scripts/intializer.py
@@ -0,0 +1,11 @@
+# initializer.py
+"""
+Basic introductory Python script.
+"""
+
+def main():
+    print("Welcome to the Super Duper Fortnight project!")
+    print("This script serves as an entry point for initialization.")
+
+if __name__ == "__main__":
+    main()
